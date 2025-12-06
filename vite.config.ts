6ea9874bcf80023564c8f451b2394980757a66b6
Shalombay7 <shalombay7@gmail.com>
2025-12-06 05:48:59 -0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // This will load .env, .env.local, .env.[mode], .env.[mode].local
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [react()],
-    define: {
-      // If you are using a library that requires process.env.API_KEY, this is the way to do it.
-      // This makes the key available to the client-side code.
-      'process.env.API_KEY': JSON.stringify(env.API_KEY || process.env.API_KEY)
-    }
-  };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
