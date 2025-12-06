6f8bd446d48afa7c188a7da6e46694442baa9b23
Shalombay7 <shalombay7@gmail.com>
2025-12-06 06:07:12 -0800

    Resolving the CSS issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        // Injects the bundled assets into the <head> of your index.html
+        tags: [
+          {
+            tag: 'script',
+            attrs: {
+              type: 'module',
+              src: '/src/index.tsx', // The plugin will resolve this correctly
+            },
+          },
+        ],
+      },
+    }),
+  ],
 })
