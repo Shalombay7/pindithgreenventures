f044df15d530fb282322103b212d05662057262c
Shalombay7 <shalombay7@gmail.com>
2025-12-06 06:15:30 -0800

    refactor: Organize project into standard Vite directory structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    createHtmlPlugin({
-      inject: {
-        // Injects the bundled assets into the <head> of your index.html
-        tags: [
-          {
-            tag: 'script',
-            attrs: {
-              type: 'module',
-              src: '/src/index.tsx', // The plugin will resolve this correctly
-            },
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 })
